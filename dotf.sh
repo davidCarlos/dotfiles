e1b595da277bbe39ef094c766c42a8296c29e9eb
David Carlos <ddavidcarlos1392@gmail.com>
2015-09-05 21:26:50 -0300

    Creates bash dir in user home.
      - Adds help output.

--- dotf.sh
+++ dotf.sh
@@ -1,8 +1,26 @@
-#!/bin/bash   
+#!/bin/bash
+DF_DIR=$(pwd)
+BKPDIR=~/.dotf_bkps/
+
+actions=("help" "install" "clean" "restore")
+
+_help()
+{
+    echo "================================================================="
+    echo "./dotf.sh install {dotf}       Installs only {dotf}"
+    echo "./dotf.sh clean {dotf}         cleans from machine only {dotf}"
+    echo "================================================================="
+}
 
 action=$1
 target=$2
 
+
+if [[ $action == "help" ]]; then
+  _help
+fi
+
 if [[ -d $target ]]; then
   ./$target/$target'.sh' $action
 fi
+
