8388298e2d11311ffe7c6634535053f909e24569
David Carlos <ddavidcarlos1392@gmail.com>
2015-09-20 18:44:38 -0300

    Improves vim.sh
      - Change ~/.vim/ folder owner.

--- vim/vim.sh
+++ vim/vim.sh
@@ -19,11 +19,47 @@ manage_action()
 
 install()
 {
-    if [[  -e $HOME/.$dotfile ]]; then
-        mv "$HOME"/.$dotfile $bkp
+    if [[ -d $HOME/.vim/ ]]; then
+        mv "$HOME"/.vim/ $bkp > /dev/null
     fi
-    ln -f $path/$dotfile "$HOME"/.$dotfile
 
+    if [[  -e $HOME/.$dotfile ]]; then
+        mv "$HOME"/.$dotfile $bkp > /dev/null
+    fi
+
+    install_dependencies
+
+    ln -f $path/$dotfile "$HOME"/.$dotfile
+    user=$(whoami)
+    sudo chown -R $user:$user ~/.vim/
+    vim -c BundleInstall
+}
+
+clean()
+{
+    sudo rm -rf ~/.vim/
+    mv $bkp/.$dotfile "$HOME"/
+    mv $bkp/.vim/ "$HOME"/
+}
+
+manage_action()
+{
+    action=$1
+
+    case $action in
+      'install')
+        install ;;
+      'clean')
+        clean ;;
+    esac
+
+}
+
+install_dependencies()
+{
+    sudo apt-get update
+    sudo apt-get install silversearcher-ag exuberant-ctags vim-nox -y
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 }
 
 manage_action "$1"
