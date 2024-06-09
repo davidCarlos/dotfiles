ecc9b8be374acea0a98f5475f16daae5210212a3
David Carlos <ddavidcarlos1392@gmail.com>
2024-06-09 12:05:22 -0300

    refactor: replaces tabline with babar plugin

--- nvim/lua/david/barbar.lua
+++ nvim/lua/david/barbar.lua
@@ -1,4 +1,5 @@
 vim.g.barbar_auto_setup = false -- disable auto-setup
+
 require("barbar").setup({
 	-- WARN: do not copy everything below into your config!
 	--       It is just an example of what configuration options there are.
@@ -7,7 +8,8 @@ require("barbar").setup({
 	-- Enable/disable animations
 	animation = true,
 
-	-- Enable/disable auto-hiding the tab bar when there is a single buffer
+	-- Automatically hide the tabline when there are this many buffers left.
+	-- Set to any value >=0 to enable.
 	auto_hide = false,
 
 	-- Enable/disable current/total tabpages indicator (top right corner)
@@ -19,15 +21,15 @@ require("barbar").setup({
 	clickable = true,
 
 	-- Excludes buffers from the tabline
-	-- exclude_ft = { "javascript" },
-	-- exclude_name = { "package.json" },
+	exclude_ft = { "javascript" },
+	exclude_name = { "package.json" },
 
 	-- A buffer to this direction will be focused (if it exists) when closing the current buffer.
-	-- Valid options are 'left' (the default) and 'right'
+	-- Valid options are 'left' (the default), 'previous', and 'right'
 	focus_on_close = "left",
 
 	-- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-	hide = { extensions = true, inactive = true },
+	hide = { extensions = true, inactive = false },
 
 	-- Disable highlighting alternate buffers
 	highlight_alternate = false,
@@ -40,15 +42,21 @@ require("barbar").setup({
 
 	icons = {
 		-- Configure the base icons on the bufferline.
+		-- Valid options to display the buffer index and -number are `true`, 'superscript' and 'subscript'
 		buffer_index = false,
 		buffer_number = false,
-		button = "",
+		button = "",
 		-- Enables / disables diagnostic symbols
 		diagnostics = {
 			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
 			[vim.diagnostic.severity.WARN] = { enabled = false },
 			[vim.diagnostic.severity.INFO] = { enabled = false },
 			[vim.diagnostic.severity.HINT] = { enabled = true },
+		},
+		gitsigns = {
+			added = { enabled = true, icon = "+" },
+			changed = { enabled = true, icon = "~" },
+			deleted = { enabled = true, icon = "-" },
 		},
 		filetype = {
 			-- Sets the icon's highlight group.
@@ -60,10 +68,16 @@ require("barbar").setup({
 		},
 		separator = { left = "▎", right = "" },
 
+		-- If true, add an additional separator at the end of the buffer list
+		separator_at_end = true,
+
 		-- Configure the icons on the bufferline when modified or pinned.
 		-- Supports all the base icon options.
 		modified = { button = "●" },
-		pinned = { button = "車", filename = true, separator = { right = "" } },
+		pinned = { button = "", filename = true },
+
+		-- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+		preset = "default",
 
 		-- Configure the icons on the bufferline based on the visibility of a buffer.
 		-- Supports all the base icon options, plus `modified` and `pinned`.
@@ -87,6 +101,9 @@ require("barbar").setup({
 	-- Sets the maximum buffer name length.
 	maximum_length = 30,
 
+	-- Sets the minimum buffer name length.
+	minimum_length = 0,
+
 	-- If set, the letters for each buffer in buffer-pick mode will be
 	-- assigned based on their name. Otherwise or in case all letters are
 	-- already assigned, the behavior is to assign letters in order of
@@ -95,14 +112,17 @@ require("barbar").setup({
 
 	-- Set the filetypes which barbar will offset itself for
 	sidebar_filetypes = {
-		-- Use the default values: {event = 'BufWinLeave', text = nil}
+		-- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
 		NvimTree = true,
 		-- Or, specify the text used for the offset:
-		undotree = { text = "undotree" },
+		undotree = {
+			text = "undotree",
+			align = "center", -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+		},
 		-- Or, specify the event which the sidebar executes when leaving:
 		["neo-tree"] = { event = "BufWipeout" },
-		-- Or, specify both
-		Outline = { event = "BufWinLeave", text = "symbols-outline" },
+		-- Or, specify all three
+		Outline = { event = "BufWinLeave", text = "symbols-outline", align = "right" },
 	},
 
 	-- New buffer letters are assigned in this order. This order is
