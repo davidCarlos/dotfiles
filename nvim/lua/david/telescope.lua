d778ebbcf6a98d3858b891d312178281882219be
David Carlos <ddavidcarlos1392@gmail.com>
2024-07-20 13:23:21 -0300

    updates dotfiles

--- nvim/lua/david/telescope.lua
+++ nvim/lua/david/telescope.lua
@@ -21,12 +21,20 @@ require("telescope").setup({
 pcall(require("telescope").load_extension, "fzf")
 
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
-vim.keymap.set("n", "<leader>l", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>f", require("telescope.builtin").git_files, { desc = "Search Git Files" })
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-vim.keymap.set("n", "<leader>g", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<leader>gb", require("telescope.builtin").current_buffer_fuzzy_find)
+local telescope_bultin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>?", telescope_bultin.oldfiles, { desc = "[?] Find recently opened files" })
+vim.keymap.set("n", "<leader>l", telescope_bultin.buffers, { desc = "[ ] Find existing buffers" })
+-- find_files can be called to search all files in the repository, including git ignored files.
+-- https://stackoverflow.com/questions/71809098/how-to-include-specific-hidden-file-folder-in-search-result-when-using-telescope
+vim.keymap.set("n", "<leader>sf", function()
+	require("telescope.builtin").find_files({
+		hidden = true,
+		find_command = { "rg", "--files", "--hidden", "--no-ignore" },
+	})
+end, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>f", telescope_bultin.git_files, { desc = "Search Git Files" })
+vim.keymap.set("n", "<leader>sh", telescope_bultin.help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", telescope_bultin.grep_string, { desc = "[S]earch current [W]ord" })
+vim.keymap.set("n", "<leader>sd", telescope_bultin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set("n", "<leader>g", telescope_bultin.live_grep, { desc = "[S]earch by [G]rep" })
+vim.keymap.set("n", "<leader>gb", telescope_bultin.current_buffer_fuzzy_find)
