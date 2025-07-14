abd73b224333973d060858dddc40e7059b9e623a
David Carlos <ddavidcarlos1392@gmail.com>
2025-07-14 12:59:33 -0300

    neovim: updates tabby.lua config

---
+++ ansible/nvim/lua/david/tabby.lua
@@ -0,0 +1,38 @@
+local theme = {
+	fill = 'TabLineFill',
+	-- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
+	head = 'TabLine',
+	current_tab = 'TabLineSel',
+	tab = 'TabLine',
+	win = 'TabLine',
+	tail = 'TabLine',
+}
+require('tabby').setup({
+	line = function(line)
+		return {
+			{
+				{ '  ', hl = theme.head },
+				line.sep('', theme.head, theme.fill),
+			},
+			line.tabs().foreach(function(tab)
+				local hl = tab.is_current() and theme.current_tab or theme.tab
+				return {
+					line.sep('', hl, theme.fill),
+					tab.is_current() and '' or '󰆣',
+					tab.number(),
+					tab.name(),
+					tab.close_btn(''),
+					line.sep('', hl, theme.fill),
+					hl = hl,
+					margin = ' ',
+				}
+			end),
+			{
+				line.sep('', theme.tail, theme.fill),
+				{ '  ', hl = theme.tail },
+			},
+			hl = theme.fill,
+		}
+	end,
+	-- option = {}, -- setup modules' option,
+})
