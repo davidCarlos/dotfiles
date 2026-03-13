e1372b220d90a6c56b81d411729b9492dbd44a43
David Carlos <ddavidcarlos1392@gmail.com>
2026-03-12 22:31:21 -0300

    fix(neovim): improves lualine integration with neovim default theme

--- ansible/nvim/lua/david/lualine.lua
+++ ansible/nvim/lua/david/lualine.lua
@@ -1,9 +1,16 @@
--- Set lualine as statusline
--- See `:help lualine.txt`
+-- https://github.com/nvim-lualine/lualine.nvim/wiki/Writing-a-theme
+-- https://github.com/nvim-lualine/lualine.nvim?tab=readme-ov-file#customizing-themes
+-- Customizing neovim default theme to avoid white background.
+local custom_default = require 'lualine.themes.auto'
+custom_default.insert.c.bg = '#1e1e1e'
+custom_default.visual.c.bg = '#1e1e1e'
+custom_default.command.c.bg = '#1e1e1e'
+custom_default.inactive.c.bg = '#1e1e1e'
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "default",
+		theme = custom_default,
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "packer", "NVimTree" },
