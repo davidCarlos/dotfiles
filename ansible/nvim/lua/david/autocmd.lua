bb1d3d7f49fc07e834e6b99419e47b65db086fa4
David Carlos <ddavidcarlos1392@gmail.com>
2026-03-18 18:21:48 -0300

    Update dotfiles

--- ansible/nvim/lua/david/autocmd.lua
+++ ansible/nvim/lua/david/autocmd.lua
@@ -1,16 +1,11 @@
-notify = require("notify")
-
 -- htmldjango filetype has better hightlight support.
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "*.html", "*.jinja2" },
 	callback = function()
 		-- https://neovim.io/doc/user/api.html#nvim_exec2()
-		local filetype_table =  vim.api.nvim_exec2(":set filetype?", { output = true })
+		local filetype_table = vim.api.nvim_exec2(":set filetype?", { output = true })
 		if string.find(filetype_table.output, "jinja.html") then
 			local result = vim.api.nvim_exec2(":set filetype=htmldjango", { output = true })
-			if result then
-				notify("set filetype as htmldjango")
-			end
 		end
 	end,
 })
--- ansible/nvim/lua/david/signatures.lua
+++ ansible/nvim/lua/david/signatures.lua
@@ -1,35 +1,35 @@
-require('hover').config({
-  --- List of modules names to load as providers.
-  --- @type (string|Hover.Config.Provider)[]
-  providers = {
-    'hover.providers.diagnostic',
-    'hover.providers.lsp',
-    'hover.providers.dap',
-    'hover.providers.man',
-    'hover.providers.dictionary',
-    -- Optional, disabled by default:
-    -- 'hover.providers.gh',
-    -- 'hover.providers.gh_user',
-    -- 'hover.providers.jira',
-    -- 'hover.providers.fold_preview',
-    -- 'hover.providers.highlight',
-  },
-  preview_opts = {
-    border = 'single'
-  },
-  -- Whether the contents of a currently open hover window should be moved
-  -- to a :h preview-window when pressing the hover keymap.
-  preview_window = false,
-  title = true,
-  mouse_providers = {
-    'hover.providers.lsp',
-  },
-  mouse_delay = 1000
+require("hover").setup({
+	--- List of modules names to load as providers.
+	--- @type (string|Hover.Config.Provider)[]
+	providers = {
+		"hover.providers.diagnostic",
+		"hover.providers.lsp",
+		"hover.providers.dap",
+		"hover.providers.man",
+		"hover.providers.dictionary",
+		-- Optional, disabled by default:
+		-- 'hover.providers.gh',
+		-- 'hover.providers.gh_user',
+		-- 'hover.providers.jira',
+		-- 'hover.providers.fold_preview',
+		-- 'hover.providers.highlight',
+	},
+	preview_opts = {
+		border = "single",
+	},
+	-- Whether the contents of a currently open hover window should be moved
+	-- to a :h preview-window when pressing the hover keymap.
+	preview_window = false,
+	title = true,
+	mouse_providers = {
+		"hover.providers.lsp",
+	},
+	mouse_delay = 1000,
 })
 
-vim.keymap.set('n', 'K', function()
-  require('hover').open()
-end, { desc = 'hover.nvim (open)' })
+vim.keymap.set("n", "K", function()
+	require("hover").open()
+end, { desc = "hover.nvim (open)" })
 
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#282828" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#282828" })
