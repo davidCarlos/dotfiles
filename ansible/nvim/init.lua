557014b66fe02e5b57bd1c8d6a0ffb8580c7b65b
David Carlos <ddavidcarlos1392@gmail.com>
2026-06-22 09:31:48 -0300

    Improves nvim configuration

--- ansible/nvim/init.lua
+++ ansible/nvim/init.lua
@@ -1,36 +1,42 @@
 local gh = function(x) return 'https://github.com/' .. x end
 vim.pack.add({
-  gh("neovim/nvim-lspconfig"),
-  gh("williamboman/mason.nvim"),
-  gh("williamboman/mason-lspconfig.nvim"),
-  -- Autocompletion
-  gh("hrsh7th/nvim-cmp"),
-  gh("neovim/nvim-lspconfig"),
-  gh("hrsh7th/cmp-nvim-lsp"),
-  gh("hrsh7th/cmp-buffer"),
-  gh("hrsh7th/cmp-path"),
-  gh("hrsh7th/cmp-cmdline"),
-  gh("hrsh7th/nvim-cmp"),
-  gh("onsails/lspkind.nvim"),
-  -- LSP
-  gh("stevearc/conform.nvim"),
-  gh("j-hui/fidget.nvim"), 
-  gh("ray-x/lsp_signature.nvim"), 
-  gh("nvimtools/none-ls.nvim"), 
-  -- Snippets
-  gh("L3MON4D3/LuaSnip"),
-  gh("saadparwaiz1/cmp_luasnip"),
-  gh("rafamadriz/friendly-snippets"),
-  -- treefile explorer
-  gh("nvim-tree/nvim-tree.lua"),
-  -- fuzzy finder
-  gh("nvim-lua/plenary.nvim"), 
-  gh("nvim-telescope/telescope.nvim"),
-  -- git
-  gh("tpope/vim-fugitive"),
-  gh("lewis6991/gitsigns.nvim"),
-  -- treesiter
-  { src = gh("nvim-treesitter/nvim-treesitter"), version = "main" }
+	gh("neovim/nvim-lspconfig"),
+	gh("williamboman/mason.nvim"),
+	gh("williamboman/mason-lspconfig.nvim"),
+	-- Autocompletion
+	gh("hrsh7th/nvim-cmp"),
+	gh("neovim/nvim-lspconfig"),
+	gh("hrsh7th/cmp-nvim-lsp"),
+	gh("hrsh7th/cmp-buffer"),
+	gh("hrsh7th/cmp-path"),
+	gh("hrsh7th/cmp-cmdline"),
+	gh("hrsh7th/nvim-cmp"),
+	gh("onsails/lspkind.nvim"),
+	-- LSP
+	gh("stevearc/conform.nvim"),
+	gh("j-hui/fidget.nvim"),
+	gh("ray-x/lsp_signature.nvim"),
+	gh("nvimtools/none-ls.nvim"),
+	-- Snippets
+	gh("L3MON4D3/LuaSnip"),
+	gh("saadparwaiz1/cmp_luasnip"),
+	gh("rafamadriz/friendly-snippets"),
+	-- treefile explorer
+	gh("nvim-tree/nvim-tree.lua"),
+	-- fuzzy finder
+	gh("nvim-lua/plenary.nvim"),
+	gh("nvim-telescope/telescope.nvim"),
+	-- git
+	gh("tpope/vim-fugitive"),
+	gh("lewis6991/gitsigns.nvim"),
+	-- treesiter
+	{ src = gh("nvim-treesitter/nvim-treesitter"), version = "main" },
+	-- IA tooling
+	gh("nickjvandyke/opencode.nvim"),
+	-- UI
+	gh("kyazdani42/nvim-web-devicons"),
+	gh("nvim-lualine/lualine.nvim"),
+	gh("nanozuki/tabby.nvim")
 })
 
 require("david")
