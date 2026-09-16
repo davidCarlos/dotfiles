e59d4be64503d0e7c8e5b9a1b1e638ec4d8d9a89
David Carlos <ddavidcarlos1392@gmail.com>
2026-09-16 09:56:09 -0300

    change nvim colorscheme

--- ansible/nvim/lua/david/set.lua
+++ ansible/nvim/lua/david/set.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = ","
 
 -- colorscheme configuration
 -- vim.g.gruvbox_italic = 1
--- vim.o.termguicolors = true
--- vim.cmd.colorscheme "catppuccin-mocha"
+vim.o.termguicolors = true
+vim.cmd.colorscheme "catppuccin-mocha"
 
 -- show relative line numbers
 vim.o.relativenumber = true
